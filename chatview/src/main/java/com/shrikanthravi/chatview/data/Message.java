c8e5167f2071da5f57bfe990bebf1102545e069f
Kevin Flou <kevin@seeyoucloud.com>
2018-09-05 09:16:16 +0200

    Ajout du rightsimplemessage

--- chatview/src/main/java/com/shrikanthravi/chatview/data/Message.java
+++ chatview/src/main/java/com/shrikanthravi/chatview/data/Message.java
@@ -45,6 +45,7 @@ public class Message {
 
     public enum MessageType{
         LeftSimpleMessage,
+        RightSimpleMessage,
         RightSimpleImage,
         LeftSingleImage,
         RightSingleImage,
