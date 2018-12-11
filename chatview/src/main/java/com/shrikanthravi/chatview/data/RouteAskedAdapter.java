13c60785dc24197948a8207709594bd2fd14a3da
Kevin Flou <kevin@seeyoucloud.com>
2018-12-11 16:46:56 +0100

    Correction layout et color

--- chatview/src/main/java/com/shrikanthravi/chatview/data/RouteAskedAdapter.java
+++ chatview/src/main/java/com/shrikanthravi/chatview/data/RouteAskedAdapter.java
@@ -37,7 +37,7 @@ public class RouteAskedAdapter extends RecyclerView.Adapter<RouteAskedAdapter.Ro
     public void onBindViewHolder(RouteAskedAdapter.RouteAskedViewHolder holder, int position) {
         RouteAsked routeAsked = listRoutes.get(position);
         holder.ligneNameTV.setText(routeAsked.getLong_name_route());
-        holder.lineNbr.setBackgroundColor(Color.parseColor(routeAsked.getColor()));
+        holder.lineNbr.setBackgroundColor(Color.parseColor("#"+routeAsked.getColor()));
         holder.lineNbr.setText(routeAsked.getShort_name_route());
         if(routeAsked.getType_route().equals("3")){
             holder.typeImageView.setImageDrawable(context.getResources().getDrawable(R.drawable.picto_bus_black));
